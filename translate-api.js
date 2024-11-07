56a0f356735e8f677322b936913b194f42713861
luckzhangfengbo <1819067326@qq.com>
2024-11-07 12:37:54 +0800

    1、添加快捷键操作

--- translate-api.js
+++ translate-api.js
@@ -1,9 +1,15 @@
 const axios = require('axios')
 const vscode = require('vscode')
 const md5 = require('md5')
-const appid = vscode.workspace.getConfiguration().get('translate.appid')
-const secret = vscode.workspace.getConfiguration().get('translate.secret')
+// const appid = vscode.workspace.getConfiguration().get('translate.appid')
+// const secret = vscode.workspace.getConfiguration().get('translate.secret')
 
+// 提供默认的 appid 和 secret，如果没有在设置中配置，会使用这些默认值
+const defaultAppid = '20251019002179935'; // 替换成你的默认 appid
+const defaultSecret = '3vZ_dKFqxaSnvMlbiuLO'; // 替换成你的默认 secret
+// 优先从用户配置获取 appid 和 secret，若未设置则使用默认值
+const appid = vscode.workspace.getConfiguration().get('translate.appid') || process.env.BAIDU_TRANSLATE_APPID || defaultAppid;
+const secret = vscode.workspace.getConfiguration().get('translate.secret') || process.env.BAIDU_TRANSLATE_SECRET || defaultSecret;
 module.exports = {
   /**
    * 翻译方法
