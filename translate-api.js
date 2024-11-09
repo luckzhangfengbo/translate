5a02d99354119949bab5f55a10fb17ed93ff3bf2
luckzhangfengbo <1819067326@qq.com>
2024-11-09 17:34:22 +0800

    删除以前版本

--- translate-api.js
+++ translate-api.js
@@ -1,15 +1,29 @@
-const axios = require('axios')
-const vscode = require('vscode')
-const md5 = require('md5')
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: luckzhangfengbo
+ * @Date: 2024-11-09 09:37:35
+ * @LastEditors: zhangfengbo
+ * @LastEditTime: 2024-11-09 09:47:17
+ */
+const axios = require("axios");
+const vscode = require("vscode");
+const md5 = require("md5");
 // const appid = vscode.workspace.getConfiguration().get('translate.appid')
 // const secret = vscode.workspace.getConfiguration().get('translate.secret')
 
 // 提供默认的 appid 和 secret，如果没有在设置中配置，会使用这些默认值
-const defaultAppid = '20251019002179935'; // 替换成你的默认 appid
-const defaultSecret = '3vZ_dKFqxaSnvMlbiuLO'; // 替换成你的默认 secret
+const defaultAppid = "20251019002179935"; // 替换成你的默认 appid
+const defaultSecret = "3vZ_dKFqxaSnvMlbiuLO"; // 替换成你的默认 secret
 // 优先从用户配置获取 appid 和 secret，若未设置则使用默认值
-const appid = vscode.workspace.getConfiguration().get('translate.appid') || process.env.BAIDU_TRANSLATE_APPID || defaultAppid;
-const secret = vscode.workspace.getConfiguration().get('translate.secret') || process.env.BAIDU_TRANSLATE_SECRET || defaultSecret;
+const appid =
+  vscode.workspace.getConfiguration().get("translate.appid") ||
+  process.env.BAIDU_TRANSLATE_APPID ||
+  defaultAppid;
+const secret =
+  vscode.workspace.getConfiguration().get("translate.secret") ||
+  process.env.BAIDU_TRANSLATE_SECRET ||
+  defaultSecret;
 module.exports = {
   /**
    * 翻译方法
@@ -26,18 +40,18 @@ module.exports = {
 }>} Promise翻译结果
    */
   translate(q, from, to) {
-    var salt = Math.random()
+    var salt = Math.random();
     return axios({
-      method: 'get',
-      url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+      method: "get",
+      url: "https://fanyi-api.baidu.com/api/trans/vip/translate",
       params: {
         q,
         appid,
         from,
         to,
         salt,
-        sign: md5(appid + q + salt + secret)
-      }
-    })
-  }
-}
+        sign: md5(appid + q + salt + secret),
+      },
+    });
+  },
+};
